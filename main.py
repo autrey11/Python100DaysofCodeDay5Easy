6fcd51cd82634e11590979b381fe65aea9c2995a
Austin Autrey <104531551+autrey11@users.noreply.github.com>
2022-05-02 19:49:09 +0000

    Randomized password

--- main.py
+++ main.py
@@ -9,16 +9,20 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+#Hard Leve- Order of characters randomised:
+#e.g. 4 letter, 2 symbols, 2 number = g^2jk8&p
+
+password_list = []
+for char in range(1, nr_letters + 1):
+   password_list.append(random.choice(letters))
+for num in range(1, nr_numbers + 1):
+  password_list.append(random.choice(numbers))
+for sym in range(1, nr_symbols + 1):
+  password_list.append(random.choice(symbols))
+
+random.shuffle(password_list)
 
 password = ""
-for char in range(1, nr_letters + 1):
-  password += random.choice(letters)
-for num in range(1, nr_numbers + 1):
-  password += random.choice(numbers)
-for sym in range(1, nr_symbols + 1):
-  password += random.choice(symbols)
-print(password)
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+for char in password_list:
+  password += char
+print(f"Your password is {password}")
